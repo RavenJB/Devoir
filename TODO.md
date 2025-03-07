1a02ae6cc96c892840d2b0adb0eeb999dd69881f
baron jules <jules.baron26@gmail.com>
2025-03-07 10:46:51 +0100

    finition du tofo

--- TODO.md
+++ TODO.md
@@ -4,12 +4,19 @@
 
 Liste des taches faites :
 
-1. Création d'un système de connexion avec profil avec visualisation du profol ou on voit le role et les resultats (partiellement).
-1.5. Possibilité de créer un role eleve/enseignant/parent et de voir ses enfants/élèves.
-2. Utilisation de la bibliothèque monolog pour les logs.
+1. Création d'un système de connexion avec profil avec visualisation du profol ou on voit le role et les resultats.
+
+
+    1.5. Possibilité de créer un role eleve/enseignant/parent et de voir ses enfants/élèves.
+
+
+2. Utilisation de la 
+bibliothèque monolog pour les logs.
+
+
 3. Creation d'une base de données faites et relié au projet avec le point 1.
 4. 0
 5. 0
-6. Essaie d'un text-to-speech avec une API qui peut créer un fichier mp3 a partir d'un texte mais non relié au projet.
+6. Text-To-Speech fait.
 7. Documentation complète du projet faite.
 
--- textToSpeech.py
+++ textToSpeech.py
@@ -1,18 +1,22 @@
 import asyncio
 import edge_tts
 
+async def text_to_speech(text: str, voice: str, output_file: str) -> None:
+    communication = edge_tts.Communicate(text, voice)
+    await communication.save(output_file+".mp3")
+    print("Le fichier audio a été créé avec succès !")
 
-TEXT = "Bonjour, comment ça va ?"
-VOICE = "fr-FR-HenriNeural"
-OUTPUT_FILE = "audio.mp3"
+def main():
+    text = input("Entrez le texte à convertir en parole : ")
+    output_file = input("Entrez le nom du fichier de sortie (avec extension .mp3) : ")
+    voice = "fr-FR-HenriNeural"  
 
-async def amain() -> None:
-    communication = edge_tts.Communicate(TEXT, VOICE)
-    await communication.save(OUTPUT_FILE)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(text_to_speech(text, voice, output_file))
+    finally:
+        loop.close()
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-try:
-    loop.run_until_complete(amain())
-finally:
-    loop.close()
+if __name__ == "__main__":
+    main()
